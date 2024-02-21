e576d724dd66009c3b6bfb15efb8c22526f2ba8d
dai <sugerbig15@gmail.com>
2024-02-21 18:22:52 +0900

    確認

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   has_many :tasks, dependent: :destroy
-  validates :name, presence:true
-  validates :email, presence:true, uniqueness:true, format:{ with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true, format:{ with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   before_validation { email.downcase! }
   has_secure_password
-  validates :password, length:{ minimum:6 }
+  validates :password, presence: true, length:{ minimum:6 }
 end
